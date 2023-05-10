f65b73bd1876009c36350533e27ee325f8768f9f
Maria Ayelen Leiva <mariaayelenleiva@gmail.com>
2023-05-10 00:52:25 -0300

    back hecho

--- Proyectointegrador/Frontend/src/app/service/skill.service.ts
+++ Proyectointegrador/Frontend/src/app/service/skill.service.ts
@@ -3,6 +3,7 @@ import { identifierName } from '@angular/compiler';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Skill } from '../model/skill';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
